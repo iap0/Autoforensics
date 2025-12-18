573bdb92f24445e7b0836493a2cd1bbb884968f8
Avaneesh Pandey <avaneesh.btmtcs2128@nfsu.ac.in>
2025-12-18 12:03:46 +0530

    16 commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://autoforensics-backend.onrender.com';
+const API_BASE_URL = 'https://autoforensics-backend.onrender.com/api';
 
 // Create axios instance with default config
 const apiClient = axios.create({
